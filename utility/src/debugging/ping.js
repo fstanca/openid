e5f15aefeac24948bc4c908a7b7f2869c75870e6
Florin Stanca <florin.stanca@itsmartsystems.eu>
2025-03-12 13:48:07 +0200

    Delete utility directory

--- utility/src/debugging/ping.js
+++
@@ -1,5 +0,0 @@
-// @ozair @spoon
-var out = { "message": "Greeting! Hello World" };
-out.whoami = apim.getContext('api.endpoint.address');
-session.output.write(JSON.stringify(out));
-apim.output("application/json");
--- utility/src/scope/provider-scope-check.js
+++
@@ -1,2 +0,0 @@
-apim.setvariable('message.headers.x-selected-scope', 'weather california');
-apim.setvariable('message.status.code', 200);
